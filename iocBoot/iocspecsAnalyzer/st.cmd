2bfd4866634e7a34857ad777337f65779b18d656
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-27 20:06:05 -0500

    Fixed autosave

--- iocBoot/iocspecsAnalyzer/st.cmd
+++ iocBoot/iocspecsAnalyzer/st.cmd
@@ -5,21 +5,27 @@
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
 epicsEnvSet("EPICS_CA_ADDR_LIST"      , "10.23.0.255")
 
-## Register all support components
 dbLoadDatabase("../../dbd/specs.dbd",0,0)
 specs_registerRecordDeviceDriver(pdbbase) 
+
+epicsEnvSet("PREFIX",	"XF:23ID1-ES{SPECS}")
+epicsEnvSet("PORT",     "SPECS")
+epicsEnvSet("QSIZE",    "20")
+epicsEnvSet("XSIZE",    "2048")
+epicsEnvSet("YSIZE",    "2048")
+epicsEnvSet("NCHANS",   "2048")
 
 drvAsynIPPortConfigure("SPECS_ASYN", "10.23.0.10:9999")
 asynSetTraceMask("SPECS_ASYN", 0, 0x9)
 asynSetTraceIOMask("P1", 0, 0x2)
 
-# Change into TOP
-cd("$(TOP)")
+specsAnalyserConfig("$(PORT)", "SPECS_ASYN", 0, 0)
 
-specsAnalyserConfig("SPECS1", "SPECS_ASYN", 0, 0)
+dbLoadRecords("$(TOP)/db/specsAnalyzer.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(TOP)/db/specsAnalyzerDLD.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
-< $(AREADETECTOR)/commonPlugins.cmd
+< $(AREA_DETECTOR)/iocBoot/commonPlugins.cmd
 
 iocInit()
 
-
+create_monitor_set("auto_settings.req", 30, "P=$(PREFIX),D=cam1:")
